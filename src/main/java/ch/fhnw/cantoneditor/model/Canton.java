9137bf53432ba2073d8e0f3224f6c6fe3276e102
aersamkull <ehrsam@kull.ch>
2015-04-21 13:30:07 +0200

    Added Property Change Support

--- src/main/java/ch/fhnw/cantoneditor/model/Canton.java
+++ src/main/java/ch/fhnw/cantoneditor/model/Canton.java
@@ -2,7 +2,7 @@ package ch.fhnw.cantoneditor.model;
 
 import java.util.List;
 
-public class Canton {
+public class Canton extends BaseModel {
     private String name;
     private String shortCut;
     private int cantonNr;
@@ -18,5 +18,149 @@ public class Canton {
     private int nrCommunes;
 
     private List<Commune> communes;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name != this.name) {
+            Object oldValue = this.name;
+            this.name = name;
+            this.pcs.firePropertyChange("name", oldValue, name);
+        }
+    }
+
+    public String getShortCut() {
+        return shortCut;
+    }
+
+    public void setShortCut(String shortCut) {
+        if (shortCut != this.shortCut) {
+            Object oldValue = this.shortCut;
+            this.shortCut = shortCut;
+            this.pcs.firePropertyChange("shortCut", oldValue, shortCut);
+        }
+    }
+
+    public int getCantonNr() {
+        return cantonNr;
+    }
+
+    public void setCantonNr(int cantonNr) {
+        if (cantonNr != this.cantonNr) {
+            Object oldValue = this.cantonNr;
+            this.cantonNr = cantonNr;
+            this.pcs.firePropertyChange("cantonNr", oldValue, cantonNr);
+        }
+    }
+
+    public int getNrCouncilSeats() {
+        return nrCouncilSeats;
+    }
+
+    public void setNrCouncilSeats(int nrCouncilSeats) {
+        if (nrCouncilSeats != this.nrCouncilSeats) {
+            Object oldValue = this.nrCouncilSeats;
+            this.nrCouncilSeats = nrCouncilSeats;
+            this.pcs.firePropertyChange("nrCouncilSeats", oldValue, nrCouncilSeats);
+        }
+    }
+
+    public int getEntryYear() {
+        return entryYear;
+    }
+
+    public void setEntryYear(int entryYear) {
+        if (entryYear != this.entryYear) {
+            Object oldValue = this.entryYear;
+            this.entryYear = entryYear;
+            this.pcs.firePropertyChange("entryYear", oldValue, entryYear);
+        }
+    }
+
+    public double getNrForeigners() {
+        return nrForeigners;
+    }
+
+    public void setNrForeigners(double nrForeigners) {
+        if (nrForeigners != this.nrForeigners) {
+            Object oldValue = this.nrForeigners;
+            this.nrForeigners = nrForeigners;
+            this.pcs.firePropertyChange("nrForeigners", oldValue, nrForeigners);
+        }
+    }
+
+    public List<Integer> getLanguageId() {
+        return languageId;
+    }
+
+    public void setLanguageId(List<Integer> languageId) {
+        if (languageId != this.languageId) {
+            Object oldValue = this.languageId;
+            this.languageId = languageId;
+            this.pcs.firePropertyChange("languageId", oldValue, languageId);
+        }
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        if (capital != this.capital) {
+            Object oldValue = this.capital;
+            this.capital = capital;
+            this.pcs.firePropertyChange("capital", oldValue, capital);
+        }
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        if (area != this.area) {
+            Object oldValue = this.area;
+            this.area = area;
+            this.pcs.firePropertyChange("area", oldValue, area);
+        }
+    }
+
+    public double getInHabitantDensity() {
+        return inHabitantDensity;
+    }
+
+    public void setInHabitantDensity(double inHabitantDensity) {
+        if (inHabitantDensity != this.inHabitantDensity) {
+            Object oldValue = this.inHabitantDensity;
+            this.inHabitantDensity = inHabitantDensity;
+            this.pcs.firePropertyChange("inHabitantDensity", oldValue, inHabitantDensity);
+        }
+    }
+
+    public int getNrCommunes() {
+        return nrCommunes;
+    }
+
+    public void setNrCommunes(int nrCommunes) {
+        if (nrCommunes != this.nrCommunes) {
+            Object oldValue = this.nrCommunes;
+            this.nrCommunes = nrCommunes;
+            this.pcs.firePropertyChange("nrCommunes", oldValue, nrCommunes);
+        }
+    }
+
+    public List<Commune> getCommunes() {
+        return communes;
+    }
+
+    public void setCommunes(List<Commune> communes) {
+        if (communes != this.communes) {
+            Object oldValue = this.communes;
+            this.communes = communes;
+            this.pcs.firePropertyChange("communes", oldValue, communes);
+        }
+    }
 
 }
--- src/main/java/ch/fhnw/cantoneditor/model/Commune.java
+++ src/main/java/ch/fhnw/cantoneditor/model/Commune.java
@@ -1,5 +1,112 @@
 package ch.fhnw.cantoneditor.model;
 
-public class Commune {
+import java.util.Date;
+
+public class Commune extends BaseModel {
+    private String shortCut;
+
+    private int districtNr;
+    private int bfsCommuneNr;
+    private String officialName;
+    private String name;
+    private String shortDistrictName;
+    private String cantonName;
+    private Date lastChanged;
+
+    public String getShortCut() {
+        return shortCut;
+    }
+
+    public void setShortCut(String shortCut) {
+        if (shortCut != this.shortCut) {
+            Object oldValue = this.shortCut;
+            this.shortCut = shortCut;
+            this.pcs.firePropertyChange("shortCut", oldValue, shortCut);
+        }
+    }
+
+    public int getDistrictNr() {
+        return districtNr;
+    }
+
+    public void setDistrictNr(int districtNr) {
+        if (districtNr != this.districtNr) {
+            Object oldValue = this.districtNr;
+            this.districtNr = districtNr;
+            this.pcs.firePropertyChange("districtNr", oldValue, districtNr);
+        }
+    }
+
+    public int getBfsCommuneNr() {
+        return bfsCommuneNr;
+    }
+
+    public void setBfsCommuneNr(int bfsCommuneNr) {
+        if (bfsCommuneNr != this.bfsCommuneNr) {
+            Object oldValue = this.bfsCommuneNr;
+            this.bfsCommuneNr = bfsCommuneNr;
+            this.pcs.firePropertyChange("bfsCommuneNr", oldValue, bfsCommuneNr);
+        }
+    }
+
+    public String getOfficialName() {
+        return officialName;
+    }
+
+    public void setOfficialName(String officialName) {
+        if (officialName != this.officialName) {
+            Object oldValue = this.officialName;
+            this.officialName = officialName;
+            this.pcs.firePropertyChange("officialName", oldValue, officialName);
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name != this.name) {
+            Object oldValue = this.name;
+            this.name = name;
+            this.pcs.firePropertyChange("name", oldValue, name);
+        }
+    }
+
+    public String getShortDistrictName() {
+        return shortDistrictName;
+    }
+
+    public void setShortDistrictName(String shortDistrictName) {
+        if (shortDistrictName != this.shortDistrictName) {
+            Object oldValue = this.shortDistrictName;
+            this.shortDistrictName = shortDistrictName;
+            this.pcs.firePropertyChange("shortDistrictName", oldValue, shortDistrictName);
+        }
+    }
+
+    public String getCantonName() {
+        return cantonName;
+    }
+
+    public void setCantonName(String cantonName) {
+        if (cantonName != this.cantonName) {
+            Object oldValue = this.cantonName;
+            this.cantonName = cantonName;
+            this.pcs.firePropertyChange("cantonName", oldValue, cantonName);
+        }
+    }
+
+    public Date getLastChanged() {
+        return lastChanged;
+    }
+
+    public void setLastChanged(Date lastChanged) {
+        if (lastChanged != this.lastChanged) {
+            Object oldValue = this.lastChanged;
+            this.lastChanged = lastChanged;
+            this.pcs.firePropertyChange("lastChanged", oldValue, lastChanged);
+        }
+    }
 
 }
