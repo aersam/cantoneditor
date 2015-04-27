71f4c8ce1173dfba0f7d5b6731193c73174875fb
aersamkull <ehrsam@kull.ch>
2015-04-27 12:27:03 +0200

    Some sample Translations & more Docu

--- src/main/java/ch/fhnw/cantoneditor/views/Overview.java
+++ src/main/java/ch/fhnw/cantoneditor/views/Overview.java
@@ -16,7 +16,6 @@ public class Overview {
         frame.setLayout(new GridLayout(3, 2));
         frame.add(new JButton(tm.Translate("SampleButton", "Just a text")));
         frame.setBounds(new Rectangle(500, 200));
-
         frame.setVisible(true);
     }
 }
--- src/main/java/ch/fhnw/cantoneditor/views/TranslationManager.java
+++ src/main/java/ch/fhnw/cantoneditor/views/TranslationManager.java
@@ -7,11 +7,18 @@ import java.io.PrintWriter;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
+/** Helper class for translation the Application */
 public class TranslationManager {
     private static TranslationManager instance;
 
+    /**
+     * This indicates that not existing values should be inserted into
+     * ApplicationTranslation.properties file. This is only meaningful during development, we should
+     * set this to false before release.
+     * */
     public static final boolean WRITE_PROPERTIES = true;
 
+    /** Gets the TranslationManager Default Instance */
     public static TranslationManager getInstance() {
         if (instance == null)
             instance = new TranslationManager();
@@ -22,14 +29,19 @@ public class TranslationManager {
 
     }
 
+    /** Gets the Translation for the given key */
     public String Translate(String key) {
         return Translate(key, key);
     }
 
+    /**
+     * Gets the Translation for the given key. If the key is not found, defaultValue will be
+     * returned
+     */
     public String Translate(String key, String defaultValue) {
         Locale locale = Locale.getDefault();
         ResourceBundle rb = ResourceBundle.getBundle("ApplicationTranslation", locale);
-        if (rb.containsKey(key)) {
+        if (rb.containsKey(key)) {// Everything is fine, just return it
             return rb.getString(key);
         } else {
 
