ab27f3013db2e4c25ec572d4051dd49aa810f229
aersamkull <ehrsam@kull.ch>
2015-06-02 10:38:56 +0200

    Fixes unselect bug in tablemodel

--- src/main/java/ch/fhnw/cantoneditor/model/CantonTableSelectionModel.java
+++ src/main/java/ch/fhnw/cantoneditor/model/CantonTableSelectionModel.java
@@ -24,7 +24,6 @@ public class CantonTableSelectionModel extends DefaultListSelectionModel {
                     return;
                 }
                 if (isSelectionEmpty()) {
-                    currentCanton.set(null);
                     return;
                 }
                 int index = getMaxSelectionIndex();
